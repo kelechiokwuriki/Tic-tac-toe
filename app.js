8ed1ef1646858a1f970d131a214629211639494e
kelechi okwuriki <kelechiokwuriki@gmail.com>
2017-08-30 04:17:31 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+function startGame() {
+    document.turn = "X";
+    setMessage(document.turn + " get's to start");
+    
+}
+
+function setMessage(msg) {
+    document.getElementById("message").innerText = msg;
+}
+
+function nextMove(square) {
+    if(square.innerText == '') {
+        square.innerText = document.turn;
+        switchTurn();
+    }
+    else {
+        setMessage("Pick another square");
+    }
+    
+}
+
+function disableBoard() {
+    var boards = document.querySelectorAll(".square");
+    
+    boards.forEach(function(board) {
+        board.innerText = "";
+    })
+    
+}
+
+function switchTurn() {
+    if(checkForWinner(document.turn)) {
+        setMessage("Congrats " + document.turn + ", you won!");
+        disableBoard();
+        //disable board and call restart function
+    }
+    else if(document.turn == "X") {
+        document.turn = "O";
+        setMessage("It's " + document.turn + "'s turn.");
+    }
+    else {
+        document.turn = "X";
+        setMessage("It's " + document.turn + "'s turn.");
+    }
+    
+}
+
+function checkForWinner(move) {
+    var result = false;
+    if(checkRow(1, 2, 3, move) ||
+      checkRow(4, 5, 6, move) ||
+      checkRow(7, 8, 9, move) ||
+      checkRow(1, 4, 7, move) ||
+      checkRow(2, 5, 8, move) ||
+      checkRow(3, 6, 9, move) ||
+      checkRow(1, 5, 9, move) ||
+      checkRow(3, 5, 3, move)) {
+        result = true;
+    }
+    return result;
+}
+
+function checkRow(a, b, c, move) {
+    var result = false;
+    if(getBox(a) == move && getBox(b) == move && getBox(c) == move) {
+        result = true;
+    }
+    return result;
+}
+
+function getBox(number) {
+    return document.getElementById("s" + number).innerText;
+}
